e0d6285e17c54dd539e97e93e50c32d960915471
Pablo Garcia <pgarciag566@gmail.com>
2022-06-28 10:49:34 -0500

    Added a new model caled posts (contains post data)

---
+++ models/Posts.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Posts extends Model { }
+
+Posts.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        //Author
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                table: "user",
+                field: "id",
+            }
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName:"posts"
+    }
+);
+
+module.exports = Posts;
