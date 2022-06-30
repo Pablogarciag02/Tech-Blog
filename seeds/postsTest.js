ab50b288d8e00e8b6dd2be6a3fb443a5ee30e032
Pablo Garcia <pgarciag566@gmail.com>
2022-06-30 12:43:26 -0500

    test works, when hardcoded

--- seeds/postsTest.js
+++ seeds/postsTest.js
@@ -2,7 +2,7 @@ const { Pages, Posts } = require("../models");
 
 const postData =[
     { 
-        title: "¿Porque Abba es una de las mejores bandas?",
+        title: "Porque Abba es una de las mejores bandas?",
         content: "Abba es una banda que origina de suecia, ellos ahora ya estan viejos. Sin embargo su musica es inolvidable. Cabe recalcar tambien, que hacen conciertos en los cuales puedes ver a los cantantes cuando ellos estaban joves gracias a technologia holograma; lo cual los hace innovadores y muy exitosos.",
         user_id: 1,
     }
