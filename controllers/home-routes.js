3f11c92411cf9de099891ed13715642a058bd32d
Pablo Garcia <pgarciag566@gmail.com>
2022-06-30 12:43:41 -0500

    for now home routes displays hardcoded posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const { User, Posts } = require("../models");
 
-router.get("/", async(req, res) => {
-    res.render("homepage", {
-        loggedIn: req.session.loggedIn,
-    });
-});
+// router.get("/", async(req, res) => {
+//     res.render("homepage", {
+//         loggedIn: req.session.loggedIn,
+//     });
+// });
 
 router.get("/login", (req, res) => {
     if(req.session.loggedIn) {
@@ -25,27 +25,51 @@ router.get("/", async (req, res) => {
                 },
             ],
         })
+        // .then(posts => {
+        //     console.log(posts + "1");
+        // }).then(dbPosts => {
+        //     console.log(dbPosts + "2");
+        // }) .catch(err =>{
+        //     console.log(err)
+        // })
 
-        const users = dbUserData.map((username) => 
-            username.get({ plain: true })
-        );
+        console.log(dbPosts);
+
+        // const users = dbUserData.map((user) => 
+        //     user.get({ plain: true })
+        // );
 
         const posts = dbPosts.map((posts) => 
             posts.get({ plain: true })
         );
 
-        console.log(users);
+        // console.log(users);
         console.log(posts);
         res.render("homepage", {
-            username,
+            // users,
             posts,
+            pageTitle: "Tech-Blog",
+            loggedIn: req.session.loggedIn,
+            login: true,
+            hasPosts: posts.length > 0
 
         });
+        // return users, posts;
+
+
+      
     }
+   
+    
     catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
+
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json(err);
+    // })
 
 
 });
