092ff8f15f9c28f85bbd32a575d7c15fe29d9ecc
Pablo Garcia <pgarciag566@gmail.com>
2022-07-08 10:46:57 -0500

    added and removed code from static js files

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,28 +1,28 @@
-// async function newFormHandler(event) {
-//     event.preventDefault();
+async function newFormHandler(event) {
+    event.preventDefault();
 
-//     const title = document.querySelector('textarea[name="title"]').value;
-//     const content = document.querySelector('textarea[name="content"]').value;
+    const title = document.querySelector('textarea[name="title"]').value;
+    const content = document.querySelector('textarea[name="content"]').value;
 
-//     console.log(title);
-//     console.log(content);
+    console.log(title);
+    console.log(content);
     
-//     const response = await fetch(`/api/posts`, {
-//         method: 'POST',
-//         body: JSON.stringify({
-//         title,
-//         content
-//         }),
-//         headers: {
-//         'Content-Type': 'application/json'
-//         }
-//     });
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+        title,
+        content
+        }),
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    });
 
-//     if (response.ok) {
-//         document.location.replace('/dashboard/');
-//     } else {
-//         alert(response.statusText);
-//     }
-// }
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
 
-// document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,19 @@
-const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
+//Logic Added to main.handlebars through script literal due to route errors.
+////////////////////////////////////////////////////////////////////////////
+// const logout = async () => {
+//   const response = await fetch('/api/users/logout', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//   });
 
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert('Failed to log out.');
-  }
-};
+//   if (response.ok) {
+//     document.location.replace('/');
+//   } else {
+//     alert('Failed to log out.');
+//   }
+// };
   
-document.querySelector('#logout').addEventListener('click', logout);
+// document.querySelector('#logout').addEventListener('click', logout);
+////////////////////////////////////////////////////////////////////////////
+
+
